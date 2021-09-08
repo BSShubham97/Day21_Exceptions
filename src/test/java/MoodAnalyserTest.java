64710e88897825787240a2d86f460e9b0a88953e
Shubham Bandagale <sbandagale18@gmail.com>
2021-09-08 23:08:02 +0530

    ADDED: enum to differentiate between MOOD ANALYSIS ERRORS

--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -1,34 +1,66 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class MoodAnalyserTest {
     @Test
     void givenMood_whenMessageIsSad_returnsSAD() {
         MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad mood");
-        String mood = moodAnalyser.analyseMood();
-        Assertions.assertEquals("SAD", mood);
+        String mood = null;
+        try {
+            mood = moodAnalyser.analyseMood();
+            Assertions.assertEquals("SAD", mood);
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+        }
     }
+
     @Test
     void givenMood_whenMessageIsHappy_returnsHappy() {
         MoodAnalyser moodAnalyser = new MoodAnalyser("I am in  Happy mood ");
-        String mood = moodAnalyser.analyseMood();
-        Assertions.assertEquals("HAPPY", mood);
+        String mood = null;
+        try {
+            mood = moodAnalyser.analyseMood();
+            Assertions.assertEquals("HAPPY", mood);
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+        }
     }
+
     @Test
     void givenMood_whenMessageIsAny_returnsHappy() {
         MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Any mood ");
-        String mood = moodAnalyser.analyseMood();
-        Assertions.assertEquals("HAPPY", mood);
+        String mood = null;
+        try {
+            mood = moodAnalyser.analyseMood();
+            Assertions.assertEquals("HAPPY", mood);
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+        }
     }
+
     /**
-     * TEST WHEN MOOD IS GIVEN AS NULL
+     * TEST WHEN MOOD MESSAGE IS GIVEN AS NULL
      */
     @Test
-    void givenMood_whenNull_returnsHAPPY() {
+    void givenMood_whenNull_returnsMessage() {
         MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood();
-        Assertions.assertEquals("HAPPY", mood);
+        try {
+            moodAnalyser.analyseMood();
+        } catch (MoodAnalysisException e) {
+            Assertions.assertEquals(MoodAnalysisException.ExceptionType.NULL_MOOD,e.type);
+            System.out.println(e.getMessage());
+        }
     }
+
+    @Test
+    void givenMessage_whenEmpty_returnsMessage() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("");
+        try {
+            moodAnalyser.analyseMood();
+        } catch (MoodAnalysisException e) {
+            Assertions.assertEquals(MoodAnalysisException.ExceptionType.EMPTY_MOOD,e.type);
+            System.out.println(e.getMessage());
+        }
+    }
+
 }
