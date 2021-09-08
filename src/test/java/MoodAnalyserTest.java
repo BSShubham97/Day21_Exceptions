9caa3c1e857503f245378196989532368148c066
Shubham Bandagale <sbandagale18@gmail.com>
2021-09-08 19:24:57 +0530

    ADDED: Repeat Test Case to run analyseMood method without pararmeters

--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -6,22 +6,22 @@ import static org.junit.jupiter.api.Assertions.*;
 class MoodAnalyserTest {
     @Test
     void givenMood_whenMessageIsSad_returnsSAD() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("I am in Sad mood");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad mood");
+        String mood = moodAnalyser.analyseMood();
         Assertions.assertEquals("SAD", mood);
     }
 
     @Test
     void givenMood_whenMessageIsHappy_returnsHappy() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("I am in  Happy mood ");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in  Happy mood ");
+        String mood = moodAnalyser.analyseMood();
         Assertions.assertEquals("HAPPY", mood);
     }
 
     @Test
     void givenMood_whenMessageIsAny_returnsHappy() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("I am in Any mood ");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Any mood ");
+        String mood = moodAnalyser.analyseMood();
         Assertions.assertEquals("HAPPY", mood);
     }
 }
